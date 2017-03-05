8f55f7a742d0a67ac875be863f960dd62f421779
Valentina Rodrigues <Valentina Rodrigues>
2017-03-04 21:46:21 -0300

    Inicio Projeto

---
+++ ProjetosGit/petshop_RM77136/app/src/main/java/desafio2/fiap/com/br/petshop_rm77136/MainActivity.java
@@ -0,0 +1,13 @@
+package desafio2.fiap.com.br.petshop_rm77136;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
